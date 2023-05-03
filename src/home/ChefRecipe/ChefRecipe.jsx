9b62e86239f4c835ec76364bfaaa64c59edad3c7
Jahid Hasan <jh3495689@gmail.com>
2023-05-03 21:53:53 +0600

    cacipe items show all data

--- src/home/ChefRecipe/ChefRecipe.jsx
+++ src/home/ChefRecipe/ChefRecipe.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import racipeBackImage from '../../assets/racipeback.jpg'
 import RacipeItem from '../RacipeItem/RacipeItem';
+import { FaThumbsUp } from 'react-icons/fa';
 
 const ChefRecipe = () => {
 
@@ -11,18 +12,20 @@ const ChefRecipe = () => {
     const { id, name, picture, racipe, years_of_experience, likes, bio, racipe_list } = chefRacipe
 
     return (
-        <div className='container mx-auto flex justify-between'>
-            <div className="bg-base-200 mt-44 w-[400px] rounded-full relative">
+        <div className='container mx-auto  mt-32 lg:flex lg:justify-between'>
+            <div className="bg-base-200 md:mb-10 lg:w-[400px] md:rounded-xl lg:rounded-full relative">
                 <div className='flex justify-center'>
-                    <img className="w-56 rounded-full absolute -mt-28" src={picture} alt="" />
+                    <img className="w-56 rounded-full absolute -mt-20" src={picture} alt="" />
                 </div>
-                <h3>{name}</h3>
-                <p>Racipe: {racipe}</p>
-                <p>Experience: {years_of_experience}</p>
-                <p>{likes}</p>
-                <p>{bio}</p>
+                <div className='lg:absolute mt-40 p-5 grid gap-3'>
+                    <h3 className='font-bold text-2xl mb-5'>{name}</h3>
+                    <p><span className='font-bold'>Racipe:</span> {racipe}</p>
+                    <p><span className='font-bold'>Experience:</span> {years_of_experience} years</p>
+                    <p className='flex items-center gap-2'><span className='font-bold'>likes:</span> <FaThumbsUp className='text-blue-500 cursor-pointer'></FaThumbsUp> {likes}</p>
+                    <p className='text-justify'><span className='font-bold'>Description</span> <br /> <span className='text-gray-600'>{bio}</span></p>
+                </div>
             </div>
-            <div className='w-[700px]'>
+            <div className='lg:w-[1000px] md:mx-auto lg:mx-0'>
                 {
                     racipe_list.map((item, index) => <RacipeItem key={index} item={item}></RacipeItem>)
                 }
--- src/home/RacipeItem/RacipeItem.jsx
+++ src/home/RacipeItem/RacipeItem.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import racipeBackImage from '../../assets/racipeback.jpg'
+import { Rating } from '@mui/material';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
 const RacipeItem = ({ item }) => {
 
-    const {name, cooking_method, ingredients, rating} = item
+    const { name, cooking_method, ingredients, rating } = item
 
     return (
-        <div className="card bg-base-100 shadow-xl image-full mb-5">
-            <figure><img src={racipeBackImage} alt="Shoes" /></figure>
+        <div className="card bg-base-100 shadow-xl image-full mb-5 h-[500px]">
+            <figure><img className='image-full w-full' src={racipeBackImage} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
+                <h2 className="card-title font-bold text-2xl mb-8">{name}</h2>
                 <p>{cooking_method}</p>
+                <p><span className='font-bold'>ingredients:</span> <br /> {ingredients}</p>
+                <div className='flex justify-between items-center'>
+                    <Rating
+                        name="simple-controlled"
+                        value={rating}
+                    />
+                    <button className='btn gap-2 text-yellow-500'><FaRegHeart className='text-lg'></FaRegHeart> favourit</button>
+                </div>
             </div>
         </div>
     );
